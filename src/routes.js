e594291eaf11de42329457e498346cfc5ec75710
Dax Kelson <dkelson@gurulabs.com>
2017-04-17 00:36:57 -0600

    More work on Add Stock Page
    Only thing left to do is hook up the global state

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import Home from './components/Home.vue';
 import Portfolio from './components/portfolio/Portfolio.vue';
 import Stocks from './components/stocks/Stocks.vue';
+import AddStock from './components/AddStock.vue';
 
 export const routes = [
     { path: '/', component: Home },
     { path: '/portfolio', component: Portfolio },
     { path: '/stocks', component: Stocks },
+    { path: '/add', component: AddStock },
 ];
