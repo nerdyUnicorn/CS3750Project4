430672bea2a961b934fa8b908a7b6855ed9d113a
nerdyUnicorn <jennifercrouch@mail.weber.edu>
2017-04-19 18:03:30 -0600

    added names to home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,15 +6,15 @@
         <hr>
         <p>Your Funds: {{ funds | currency }}</p>
         <hr>
-        <p>
+        <br>
+        <br>
+        <br>
         <h4>CREATED BY: </h4>
         <h6>Jennifer Crouch</h6>
         <h6>Zachary Fullmer</h6>
         <h6>Dax Kelson</h6>
         <h6>Justin Messenger</h6>
         <h6>Nelson Muhlestein</h6>
-        </p>
-        
     </div>
 </template>
 
