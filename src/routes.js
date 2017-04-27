c15bf7afa28059927877ffc0bb20cfa43a97b04c
zachfullmer <zacharyfullmer@mail.weber.edu>
2017-04-27 15:03:00 -0600

    User is now prevented from visiting some pages when not logged in

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,19 @@ import AddStock from './components/AddStockPage/AddStock.vue';
 import ManageStocks from './components/ManageStocksPage/ManageStocks.vue';
 import StockView from './components/StockViewPage/StockView.vue';
 import StockList from './components/StockListPage/StockList.vue';
+import {store} from './store/store.js'
+
+function redirectIfNotLoggedIn(to, from, next) {
+    if(store.getters.loginStatus === false) {
+        next('/');
+    }
+    next();
+}
 
 export const routes = [
     { path: '/', component: Home },
-    { path: '/stocklist', component: StockList },
-    { path: '/addstocks', component: AddStock },
-    { path: '/managestocks', component: ManageStocks },
-    { path: '/stockview', component: StockView },
+    { path: '/stocklist', component: StockList, beforeEnter: redirectIfNotLoggedIn },
+    { path: '/addstocks', component: AddStock, beforeEnter: redirectIfNotLoggedIn },
+    { path: '/managestocks', component: ManageStocks, beforeEnter: redirectIfNotLoggedIn },
+    { path: '/stockview', component: StockView, beforeEnter: redirectIfNotLoggedIn },
 ];
