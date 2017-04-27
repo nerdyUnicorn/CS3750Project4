0c2d50cd919d7eee979c43bc4ccfde3a584bf160
nerdyUnicorn <jennifercrouch@mail.weber.edu>
2017-04-27 16:06:32 -0600

    made list view for description of site

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,7 +5,11 @@
         <hr>
         <li>
         <h5>This sight allows you to make a customized list of stocks to follow. </h5> 
+        </li>
+        <li>
         <h5>You can view the increase/decrease of your stock list over a specified period of time.</h5>
+        </li>
+        <li>
         <h5>It also permits money allocation to desired stocks with a slide bar and pie chart.</h5>
         </li>
         <hr>
