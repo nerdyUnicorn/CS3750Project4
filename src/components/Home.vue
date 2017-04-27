686aefe0ec27fd2f40640cbb94d7bc3eb61fa578
nerdyUnicorn <jennifercrouch@mail.weber.edu>
2017-04-27 15:37:12 -0600

    home page info

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,12 @@
         <hr>
         <h1>Stock Ticker</h1>
         <hr>
+        <li>
+        <h5>This sight allows you to make a customized list of stocks to follow. </h5> 
+        <h5>You can view the increase/decrease of your stock list over a specified period of time.</h5>
+        <h5>It also permits money allocation to desired stocks with a slide bar and pie chart.</h5>
+        </li>
+        <hr>
         <br>
         <br>
         <br>
