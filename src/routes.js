be78f885f323b0321c7a6308ca2c0da0d3e04c53
nelsonmuhlestein <azrethvaloros@yahoo.com>
2017-04-27 18:13:49 -0600

    image added

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import AddStock from './components/AddStockPage/AddStock.vue';
 import ManageStocks from './components/ManageStocksPage/ManageStocks.vue';
 import StockView from './components/StockViewPage/StockView.vue';
 import StockList from './components/StockListPage/StockList.vue';
+import Page404 from './components/Page404/page404.vue';
 
 export const routes = [
     { path: '/', component: Home },
     { path: '/stocklist', component: StockList },
     { path: '/addstocks', component: AddStock },
     { path: '/managestocks', component: ManageStocks },
+    { path: '*', component: Page404 },
     { path: '/stockview', component: StockView },
 ];
